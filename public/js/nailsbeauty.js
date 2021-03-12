1f660aa9e1dbf1f3b8d9e2e8cdbd7171d80324dc
h-developpeur <hdeveloppeur@gmail.com>
2021-03-12 15:52:05 +0100

    first commit

---
+++ public/js/nailsbeauty.js
@@ -0,0 +1,30 @@
+// Pages Redirection 
+
+function fonctionAccueil() {
+    document.getElementsByClassName("accueil");
+    location.href = "file:///C:/Users/Nitro/Documents/CODAGES/NAILS%20BEAUTY/public/nailsbeauty.html";
+    }
+
+function fonctionService() {
+    document.getElementsByClassName("service");
+    location.href = "file:///C:/Users/Nitro/Documents/CODAGES/NAILS%20BEAUTY/public/nailsbeauty-service.html";
+    }
+
+function fonctionVernis() {
+    document.getElementsByClassName("vernis");
+    location.href = "file:///C:/Users/Nitro/Documents/CODAGES/NAILS%20BEAUTY/public/nailsbeauty-vernis.html";
+    }
+
+function fonctionEquipe() {
+    document.getElementsByClassName("equipe");
+    location.href = "file:///C:/Users/Nitro/Documents/CODAGES/NAILS%20BEAUTY/public/nailsbeauty-equipe.html";
+    }
+
+function fonctionTarif() {
+    document.getElementsByClassName("tarif");
+    location.href = "file:///C:/Users/Nitro/Documents/CODAGES/NAILS%20BEAUTY/public/nailsbeauty-tarif.html";
+    }
+
+
+
+
